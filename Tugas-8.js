8f23cd6f793099f38139ed4970b19001a0fbb2c4
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 09:05:08 +0700

    Tugas_8_JS
    Tugas_8_JS

---
+++ Tugas-8.js
@@ -0,0 +1,11 @@
+function Tugas() {
+    var kucing = {
+        nama: "Chiko",
+        gender: "Jantan",
+        warna: "belang abu coklat hitam"
+    }
+    kucing.umur = "3 tahun";
+    console.log(kucing);
+
+}
+Tugas()
